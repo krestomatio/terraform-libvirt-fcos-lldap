59639ce299c028c1ffecc9c427e00bfdb2767f92
Job Céspedes Ortiz <jobcespedes@gmail.com>
2023-08-17 23:12:55 -0600

    refactor: standarize hook script

--- locals.tf
+++ locals.tf
@@ -11,33 +11,35 @@ locals {
       #!/bin/bash
 
       # vars
-      lldap_cert_folder_path="${local.data_volume_path}/${local.ssl_path}"
-      lldap_cert_path="$$${lldap_cert_folder_path}/fullchain.pem"
-      lldap_key_path="$$${lldap_cert_folder_path}/privkey.pem"
-      lldap_proxy_uid="0"
-      lldap_proxy_gid="0"
+      container_name=lldap
+      cert_folder_path="${local.data_volume_path}/${local.ssl_path}"
+      cert_path="$$${cert_folder_path}/fullchain.pem"
+      key_path="$$${cert_folder_path}/privkey.pem"
+      key_path="0"
+      proxy_gid="0"
       source_cert_folder_path="/etc/letsencrypt/live/${var.external_fqdn}"
       source_cert_path="$$${source_cert_folder_path}/fullchain.pem"
       source_key_path="$$${source_cert_folder_path}/privkey.pem"
 
       # handle cert correct placement
       # dir
-      mkdir -p $$${lldap_cert_folder_path}
+      mkdir -p $$${cert_folder_path}
       # cert
-      cp -f "$$${source_cert_path}" "$$${lldap_cert_path}"
+      cp -f "$$${source_cert_path}" "$$${cert_path}"
       # key
-      cp -f "$$${source_key_path}" "$$${lldap_key_path}"
+      cp -f "$$${source_key_path}" "$$${key_path}"
       # owner
-      chown $$${lldap_proxy_uid}:$$${lldap_proxy_gid} "$$${lldap_cert_folder_path}" "$$${lldap_cert_path}" "$$${lldap_key_path}"
+      chown $$${key_path}:$$${proxy_gid} "$$${cert_folder_path}" "$$${cert_path}" "$$${key_path}"
       # permissions
-      chmod 0600 "$$${lldap_cert_path}" "$$${lldap_key_path}"
+      chmod 0600 "$$${cert_path}" "$$${key_path}"
 
       # restart container
-      if podman ps lldap &> /dev/null
+      if podman ps --format "{{.Names}}" 2> /dev/null | grep -q -w $container_name
       then
-        podman restart lldap
+        echo "restarting $container_name container..."
+        podman restart $container_name
       else
-        echo "lldap container not running"
+        echo "$container_name container not running"
       fi
     TEMPLATE
   }
